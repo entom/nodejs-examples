8209909d94d5c68ee08eb1f0f67164a0dff3e9e8
entom <tomasz@entom.info>
2021-06-21 21:21:38 +0200

    feat(events): send random number by event

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const http = require('http')
+const EventEmitter = require('events')
+const eventEmiter = new EventEmitter()
 
 const hostname = '0.0.0.0'
 const port = process.env.PORT || 3000
@@ -9,6 +11,18 @@ const server = http.createServer((req, res) => {
     res.end('Hello World :)\n')
 })
 
+eventEmiter.on('random-number', (value) => {
+    console.log('event-random-number', value)
+})
+
+const randomNumber = () => {
+    for (let i = 0; i < 10; i++) {
+        eventEmiter.emit('random-number', Math.round(Math.random() % 100 * 100))
+    }
+}
+
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`)
+
+    randomNumber()
 })
