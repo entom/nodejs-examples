4920f4ef4a9b67ec97bbaacf062d75b1470b9989
entom <tomasz@entom.info>
2021-06-23 19:37:51 +0200

    feat(routes): add 404 for any other url

--- src/server.js
+++ src/server.js
@@ -15,6 +15,9 @@ const server = http.createServer((req, res) => {
     } else if (req.url === '/status' && req.method === 'GET') {
         res.writeHead(200, {'Content-Type': 'application/json'})
         res.end(JSON.stringify({status: 'ok'}));
+    } else {
+        res.writeHead(404, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({status: 'error', message: 'Page not found'}));
     }
 })
 
